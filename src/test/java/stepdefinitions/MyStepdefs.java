f83ec9ba385e23277d989d8e22e1b650913101ec
rjumamuradov <rozymyrat.jumamuradov@gmail.com>
2022-02-26 22:12:27 +0100

    last days

--- src/test/java/stepdefinitions/MyStepdefs.java
+++ src/test/java/stepdefinitions/MyStepdefs.java
@@ -5,5 +5,6 @@ import io.cucumber.java.en.And;
 public class MyStepdefs {
     @And("tekrar kapatilir")
     public void tekrarKapatilir() {
+
     }
 }
--- src/test/java/stepdefinitions/MyStepdefs1.java
+++ src/test/java/stepdefinitions/MyStepdefs1.java
@@ -5,5 +5,6 @@ import io.cucumber.java.en.Given;
 public class MyStepdefs1 {
     @Given("ve tekrar")
     public void veTekrar() {
+
     }
 }
